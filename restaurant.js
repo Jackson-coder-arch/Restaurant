7c8be2a47ff51e427451a062b1ecdb63d75a272f
Jackson-coder-arch <jacksonikonya@gmail.com>
2021-01-13 16:06:18 +0300

    layout

---
+++ restaurant.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+    $(".design").click(function(){
+        $("#design, .design").toggle("slow");
+    });
+    $("#design").click(function(){
+        $(".design, #design").toggle("slow");
+    });
+    $(".development").click(function(){
+        $("#development, .development").toggle("slow");
+    });
+    $("#development").click(function(){
+        $(".development, #development").toggle("slow");
+    });
+    $(".product").click(function(){
+        $("#product, .product").toggle("slow");
+    });
+    $("#product").click(function(){
+        $(".product, #product").toggle("slow");
+    });
+   
+});
