aed3f217b3a00fc5025f9892ebfdd4803247bb24
Lewis Njagi <lewismaybach@gmail.com>
2021-01-14 22:41:45 +0300

    add javascript functionality

---
+++ food/food.js
@@ -0,0 +1,81 @@
+var order = [];
+
+function Food(size, total) {
+    this.foodSize = size;
+    this.foodTotal = total;
+};
+
+
+Food.prototype.nameSize = function () {
+    if (this.foodSize == 250) {
+        return "Small";
+    }
+    if (this.foodSize == 400) {
+        return "Medium"
+    }
+    if (this.foodSize == 550) {
+        return "Large"
+    }
+};
+// $("#burg1").click(function () {
+//     $(".card-text").text(foodSize);
+// });
+// var diplay = document.getElementById("#button1").addEventListener("click", function () {
+//     alert("Hello World!");
+// });
+$("#button1").click(function () {
+    var foodSize = parseInt($("#burg").val());
+    var matoke = new Food(foodSize);
+    $("#subtotal").show();
+    $(".carttext").show();
+    $(".cartsub").show();
+    $(".nproduct").hide();
+
+    
+    order.push(matoke);
+
+    var totalAmount = 0;
+    for (let i = 0; i < order.length; i++) {
+        totalAmount += order[i].foodSize;
+    };
+
+    $("ul#list").append("<li><span class='food'>" + "Burger" + "<br>" + matoke.nameSize() +
+        "<span style='padding-left: 120px;'>" + matoke.foodSize + "</span>" + "</span></li>");
+    $("h2#total").text(totalAmount);
+    if (foodSize == 0) {
+        $("#subtotal").hide();
+        $(".carttext").hide();
+        $(".cartsub").hide();
+        $("ul#list").hide();
+        $("h2#total").hide();
+        $(".nproduct").show();
+        alert("You did not make a selection!");
+    }
+});
+$("#button2").click(function () {
+    $("ul#list").append("<li><span class='food'>" + "Potato Chips" + "</span></li>");
+});
+$("#button3").click(function () {
+    $("ul#list").append("<li><span class='food'>" + "Ice Cream" + "</span></li>");
+});
+$("#button4").click(function () {
+    $("ul#list").append("<li><span class='food'>" + "Pizza" + "</span></li>");
+});
+$("#button5").click(function () {
+    $("ul#list").append("<li><span class='food'>" + "Sandwich" + "</span></li>");
+});
+$("#button6").click(function () {
+    $("ul#list").append("<li><span class='food'>" + "Fried Chicken" + "</span></li>");
+});
+$("#button7").click(function () {
+    $("ul#list").append("<li><span class='food'>" + "Onion Rings" + "</span></li>");
+});
+$("#button8").click(function () {
+    $("ul#list").append("<li><span class='food'>" + "Hot Dogs" + "</span></li>");
+});
+$("#button9").click(function () {
+    $("ul#list").append("<li><span class='food'>" + "French Fries" + "</span></li>");
+});
+$("#button10").click(function () {
+    $("ul#list").append("<li><span class='food'>" + "Tacos" + "</span></li>");
+});
