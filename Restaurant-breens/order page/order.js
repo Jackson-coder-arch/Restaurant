e208d92efc304af52db13f3a801ed2f5b6407fd3
alexgathuri <57430371+alexgathuri@users.noreply.github.com>
2021-01-15 11:05:50 +0300

    Update order.js

--- Restaurant-breens/order page/order.js
+++ Restaurant-breens/order page/order.js
@@ -1,19 +1,14 @@
 //user interface logic
 $(document).ready(function () {
-    $("#add-cart").click(function() {
-        var itemName = $("p").val();
-        var price = $("h2").val();
-        var number = $("").val();
+    $("#checkout").click(function() {
+        location.href = "order.html";
+    })
+
+    $("#order").click(function() {
         swal({
             title: "Conpleted Order! Wait for delivery",
-            text: `You bought ${number} ${itemName} ${price} and the total is ${priceTotal()}`,
+            text: `You bought and the total is`,
             icon: "success",
           });
     })
 })
-
-//backend logic
-function priceTotal() {
-    var total = itemName * number
-    return total;
-}
